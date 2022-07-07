36a9008b872b02429202a079cb3b4e8383274ba5
vladcuevas <vladcuevas@gmail.com>
2022-07-07 10:41:46 -0500

    Update EmployeeRepository.java

--- src/main/java/com/fsdprogram/demo/controllers/employee/EmployeeRepository.java
+++ src/main/java/com/fsdprogram/demo/controllers/employee/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.fsdprogram.demo.controllers.employee;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.fsdprogram.demo.model.Employee;
@@ -10,6 +11,6 @@ import com.fsdprogram.demo.model.Employee;
 @Repository("EmployeeRepository")
 interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-    List<Employee> findByNameContaining(String name);
+    List<Employee> findByNameContaining(@Param("name") String name);
 
 }
