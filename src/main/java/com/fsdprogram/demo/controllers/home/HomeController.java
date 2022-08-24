52e519977c791fa2ba941a017a1c9916c159f5c8
vladcuevas <vladcuevas@gmail.com>
2022-08-23 20:03:51 -0500

    mockup data for the medicines

--- src/main/java/com/fsdprogram/demo/controllers/home/HomeController.java
+++ src/main/java/com/fsdprogram/demo/controllers/home/HomeController.java
@@ -43,5 +43,4 @@ public class HomeController {
 
         return "redirect:/login";
     }
-
 }
--- src/main/java/com/fsdprogram/demo/model/Medicine.java
+++ src/main/java/com/fsdprogram/demo/model/Medicine.java
@@ -20,14 +20,24 @@ public class Medicine {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String name;
     @Column(nullable = false)
     private String companyName;
     @Column(nullable = false)
+    private Integer quantity;
+    @Column(nullable = false)
+    private Integer uses;
+    @Column(nullable = false)
     private BigDecimal price;
+    @Column(nullable = false)
+    private Integer discount;   
+    @Column(nullable = false)
+    private Short rating;    
     @Column(name = "disease",nullable = false)
     private String disease;
+    @Column(nullable = false)
+    private String image;
     @Column(nullable = false)
     @Temporal(TemporalType.DATE)
     private Date expirationDate;
